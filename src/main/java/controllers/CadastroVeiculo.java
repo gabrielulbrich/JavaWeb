bde327afc360afc5712f0b4e8da74bd6a1c5a52c
Gabriel Ulbrich <gabriel_ulbrich@outlook.com>
2019-03-31 18:21:40 -0300

    cadastro de veiculos

--- src/main/java/controllers/CadastroVeiculo.java
+++ src/main/java/controllers/CadastroVeiculo.java
@@ -10,13 +10,13 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/cadastro-produto")
-public class CadastroProduto extends HttpServlet {
+@WebServlet("/cadastro-veiculo")
+public class CadastroVeiculo extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         // Preprocess request: we actually don't need to do any business stuff, so just display JSP.
-    	request.setAttribute("views", "cadastro_produto.xhtml");
+    	request.setAttribute("views", "cadastro_veiculo.xhtml");
         request.getRequestDispatcher("views/index.xhtml").forward(request, response);
     }
 
